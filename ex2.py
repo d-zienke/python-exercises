3fdf7ddc082cd8550932cda3b0a7b9085a04270e
Damian Zienke <zienke.damian@gmail.com>
2024-10-01 19:51:22 +0200

    Add solution to exercise 2

---
+++ ex2.py
@@ -0,0 +1,19 @@
+"""
+Exercise 2.
+
+Verify the integer taken from the keyboard for parity. 
+Display an adequate message when it is or is not even.
+"""
+
+prompt = "Enter an integer: "
+
+user_input = input(prompt)
+try:
+    user_input_num = int(user_input)
+    is_even = user_input_num % 2 == 0
+    if is_even:
+        print(f"Number {user_input_num} is even")
+    else:
+        print(f"Number {user_input_num} is odd")
+except:
+    print("The value you entered wasn't an integer")
